c7615443f609996767348b57daf52dfea04958da
pramod <pramod.nandy06@gmail.com>
2020-09-21 22:51:35 +0200

    Create Array_FindAllChildNode.cpp
    Testcases:-
    I/p- {0,2,2,2,3} O/p- 3
    I/p- {1,2,3,4} O/p-3
    I/p- {4,2,0} O/p- 0
    I/p- {0,2,3,3,1,1} O/p-4
    I/p- {10,6,8} O/p- 0
    I/p- {5,4,3} O/p- 2

---
+++ Array_FindAllChildNode.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numberOfParentChildPair(int *a,int n)
+{
+    int H[256]={0,};
+    int count = 0;
+    for(int i=0;i<n;i++)
+    {
+        H[a[i]] = 1;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(H[a[i]+1])
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    int a[]= {1,2,3,4};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<<numberOfParentChildPair(a,n)<<endl;
+    return 0;
+}
