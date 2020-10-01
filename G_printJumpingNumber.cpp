70320252a9e73f98d1ce69fdc2179a8610d6fbf6
pramod <pramod.nandy06@gmail.com>
2020-10-01 23:08:35 +0200

    Using both DFS & BFS

--- G_printJumpingNumber.cpp
+++ G_printJumpingNumber.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 //Using BFS
-void jumpNumberUtil(int i, int k)
+void jumpNumberUtil_Bfs(int i, int k)
 {
     list<int> Q;
     Q.push_back(i);
@@ -29,17 +29,46 @@ void jumpNumberUtil(int i, int k)
         }
     }
 }
+//using dfs
+void jumpNumberUtil_Dfs(int i, int k)
+{
+    if(i<k)
+    {
+        cout<<i<<"--";
+        int last_digit = i %10;
+        if(last_digit == 0)
+        {
+            jumpNumberUtil_Dfs((i*10) + (last_digit+1),k);
+        }
+        else if(last_digit == 9)
+        {
+            jumpNumberUtil_Dfs((i*10) + (last_digit-1),k);
+        }
+        else
+        {
+            jumpNumberUtil_Dfs((i*10) + (last_digit-1),k);
+            jumpNumberUtil_Dfs((i*10) + (last_digit+1),k);
+        }
+    }
+}
+
 void jumpNumber(int k)
 {
     for(int i=1;i<=9;i++)
     {
-        jumpNumberUtil(i,k);
+        jumpNumberUtil_Bfs(i,k);
     }
     cout<<endl;
+    for(int i=1;i<=9;i++)
+    {
+        jumpNumberUtil_Dfs(i,k);
+    }
+    cout<<endl;
+
 }
 int main()
 {
-    int k[] = {20,30};
+    int k[] = {20,180};
     int n = sizeof(k)/sizeof(k[0]);
     for(int i=0;i<n;i++)
     {
