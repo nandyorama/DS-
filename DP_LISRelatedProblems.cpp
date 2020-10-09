a0bf7cd54b6783bb6698b2f24eeaef0db766596a
pramod <pramod.nandy06@gmail.com>
2020-10-09 23:46:04 +0200

    Update DP_LISRelatedProblems.cpp

--- DP_LISRelatedProblems.cpp
+++ DP_LISRelatedProblems.cpp
@@ -12,7 +12,7 @@ int LIS(int *a,int n)
         {
             if(a[i]>a[j] && result[i]<result[j]+1)
             {
-                result[i] = res[j]+1;
+                result[i] = result[j]+1;
             }
         }
     }
@@ -51,11 +51,43 @@ int MaxSUMIncreasingOrder(int * arr,int n)
     return result;
 }
 
+struct Pair  
+{ 
+    int x;  
+    int y;  
+};  
+
+int maxChain( Pair arr[], int n)  
+{  
+    int max = 0;  
+    std::vector<int> result(n,1);  
+    for (int i = 1; i < n; i++ )  
+    {
+        for (int j = 0; j < i; j++ )  
+        {
+            if ( arr[i].x > arr[j].y && result[i] < result[j] + 1)  
+                result[i] = result[j] + 1;  
+        }       
+    }
+    for (int i = 0; i < n; i++ )  
+    {
+        if ( max < result[i] )  
+            max = result[i];  
+    } 
+    return max;  
+}  
+
+
 int main()
 {
-    int a[]={4,5,1,3,4,9};
+    int a[]={1, 101, 2, 3, 100, 4, 5};
     int n = sizeof(a)/sizeof(a[0]);
-    cout<<LIS(a,n);
-    cout<<endl<<MaxSUMIncreasing(a,n);
+    cout<<LIS(a,n);//4
+    
+    cout<<endl<<MaxSUMIncreasingOrder(a,n)<<endl;//106
+    
+    Pair arr[] = { {5, 24}, {39, 60}, {15, 28}, {27, 40}, {50, 90} };  
+    n = sizeof(arr)/sizeof(arr[0]);  
+    cout << "Maximum size chain is " << maxChain(arr, n); //Output=3
     return 0;
 }
